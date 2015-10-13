86f3103a82ecbf256828b90f1c77c754f2031947
dongwt <834575475@qq.com>
2015-10-13 18:13:20 +0800

    日志目录自动创建

--- config/log.js
+++ config/log.js
@@ -1,6 +1,11 @@
 var path = require("path");
 var log4js = require('log4js');
+var fs = require('fs');
 log4js.configure(path.join(__dirname, "log4js.json"));
+
+var logPath = __dirname.replace("/config","");
+var logDirectory = logPath + '/logs';
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 exports.logger = function(name) {
     return log4js.getLogger(name);
